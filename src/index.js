0a50f6e4abb1c41286f8a5b2a748e5e8404c5f6e
Pierre-Alexandre Monneau <pierre-alexandre.monneau@epitech.eu>
2018-06-12 20:34:33 +0200

    React Router init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './views/Login/Login';
+import NotFound from './views/Error/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
+  </Router>
+  , document.getElementById('root'),
+);
 registerServiceWorker();
